70d53914852e243b558e9753440e5029272753f1
Nathan <captainlonate@gmail.com>
2024-02-08 18:17:33 +0000

    still figuring out the flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom'
-import { Header } from './Header/Header'
-import { Dashboard } from './pages/Dashboard/Dashboard'
-import { Trips } from './pages/Trips/Trips'
-import { Billing } from './pages/Billing/Billing'
-import { Debugging } from './Debugging/Debugging'
-import './App.css'
-import { AppContextProvider, useAppContext } from './contexts/app/AppContext'
-import { useEffect, useState } from 'react'
-import { getUser } from './data/getUser'
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
+import { Header } from "./Header/Header";
+import { Dashboard } from "./pages/Dashboard/Dashboard";
+import { Trips } from "./pages/Trips/Trips";
+import { Billing } from "./pages/Billing/Billing";
+import { Debugging } from "./Debugging/Debugging";
+import "./App.css";
+import { AppContextProvider, useAppContext } from "./contexts/app/AppContext";
+import { useEffect, useState } from "react";
+import { getUser } from "./data/getUser";
 
 /**
  * Still need to prove:
@@ -22,7 +29,7 @@ function PageLayout() {
       <Debugging />
       <Outlet />
     </>
-  )
+  );
 }
 
 function ActualRoutes() {
@@ -34,7 +41,7 @@ function ActualRoutes() {
 
       <Route path="*" element={<Navigate to="." />} />
     </Routes>
-  )
+  );
 }
 
 function AppRoutes() {
@@ -45,7 +52,7 @@ function AppRoutes() {
         <Route path="*" element={<ActualRoutes />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
 function AppWithAllProviders() {
@@ -53,20 +60,27 @@ function AppWithAllProviders() {
     <div className="app">
       <AppRoutes />
     </div>
-  )
+  );
 }
 
 /**
- * Arrive at the site with a url. Either:
+ * 1)
+ * User arrive at the site with a url. Either:
+ *  - /company/:companyId/
  *  - /company/:companyId/billing
  *  - /
  *  - /billing
- * 
+ *
+ * 2)
  * Show loading spinner and start running logic in <AppInit />.
- * 
+ *
+ * 3)
  * Try to log in the user or check if already logged in. Get token.
  * Parse token and find list of business Ids and/or user role.
- * 
+ * At this point, I'm still showing the loading spinner.
+ * Now I know which URL they were trying to load, and which companyIds they have access to.
+ *
+ * 4)
  * If role===superadmin
  *   Must go to company/:companyId
  *     If already going to companyId, verify it's in their list
@@ -74,39 +88,57 @@ function AppWithAllProviders() {
  *      If not in their list
  *        Trigger the company chooser modal
  *     If not already going to companyId, trigger the company chooser modal
- * 
+ *
  * If role===user
- * 
- * If user role is "superadmin", 
+ *
+ * If user role is "superadmin",
  */
 
+// Just pseudo code
+// function determineWhatToDo(
+//   intendedURL: string,
+//   role: "user" | "superadmin",
+//   companyIds: string[],
+// ): string {
+//   if (role === "superadmin") {
+//     //
+//   } else {
+//     //
+//   }
+// }
 
 /**
- * 
+ *
  */
 function AppInit({ children }: { children: React.ReactNode }) {
-  const { state: { activeBusinessId, appInitState }, dispatch } = useAppContext();
+  const { companyId } = useParams();
+  const {
+    state: { activeBusinessId, user, appInitState },
+    dispatch,
+  } = useAppContext();
+
+  console.log("AppInit", { companyId, user, appInitState });
 
   useEffect(() => {
     if (activeBusinessId || appInitState !== "default") {
       return;
     }
-    dispatch({type: 'SET_APP_INIT', payload: "pending"})
+    dispatch({ type: "SET_APP_INIT", payload: "pending" });
     // Log in the user, get back token, check role and business ids
     getUser("superadmin").then((user) => {
-      dispatch({ type: 'SET_USER', payload: { ...user } })
-      dispatch({ type: 'SET_BUSINESS_ID', payload: user.companyIds[0] })
-      dispatch({ type: 'SET_APP_INIT', payload: "success" })
-    })
-  }, [appInitState, dispatch, activeBusinessId])
+      dispatch({ type: "SET_USER", payload: { ...user } });
+      dispatch({ type: "SET_BUSINESS_ID", payload: user.companyIds[0] });
+      dispatch({ type: "SET_APP_INIT", payload: "success" });
+    });
+  }, [appInitState, dispatch, activeBusinessId]);
 
   switch (appInitState) {
     case "default":
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     case "pending":
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     case "success":
-      return activeBusinessId ? <>{children}</> : null
+      return activeBusinessId ? <>{children}</> : null;
     case "error":
       return <div>Error! No Business ID Found.</div>;
     default:
@@ -125,7 +157,7 @@ function Providers() {
         </AppInit>
       </AppContextProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default Providers
+export default Providers;
